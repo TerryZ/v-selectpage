885fbcdb73ddc46ba9baa8a9ac8a65b75e6b9ec4
Terry <terry5@foxmail.com>
2023-07-07 18:05:39 +0800

    refactor: table view

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,51 +1,51 @@
-import view from '../mixins/view'
+import { h } from 'vue'
+
+import { useList, useInject } from '../core/list'
+
 export default {
   name: 'SelectPageTable',
-  mixins: [view],
   props: {
-    tbColumns: Array
+    list: { type: Array, default: undefined },
+    tbColumns: { type: Array, default: undefined }
   },
-  render (h) {
-    return h('table', { class: 'sp-table' }, [
-      // table thead
-      h('thead', [h('tr', {
-        class: { 'sp-rtl': this.rtl }
-      }, this.tbColumns.map(val => h('th', val.title)))]),
-      // table tbody
-      h('tbody', {
-        on: {
-          mouseleave: () => this.highlight(-1)
-        }
-      }, this.list.map((val, index) => {
-        // table row
-        return h('tr', {
-          key: index,
-          class: this.rowClass(val, index),
-          on: {
-            click: e => {
-              e.stopPropagation()
-              this.rowClick(val)
-            },
-            mouseenter: () => this.highlight(this.inPicked(val) ? -1 : index)
-          }
-        }, this.tbColumns.map((col, idx) => { // table cells
-          return h('td', {
-            key: idx,
-            domProps: {
-              innerHTML: this.renderColumn(val, col)
-            }
-          })
-        }))
-      }))
-    ])
-  },
-  methods: {
-    renderColumn (row, col) {
+  setup (props, { emit }) {
+    const { itemSelect, setItemHighlight, itemClasses } = useList(props, emit)
+    const { isPicked, rtl } = useInject()
+
+    const renderColumn = (row, col) => {
       if (!row || !Object.keys(row).length || !col || !col.data) return ''
       switch (typeof col.data) {
         case 'string': return row[col.data]
         case 'function': return col.data(row)
       }
+    }
+
+    return () => {
+      const thCells = props.tbColumns.map(val => h('th', val.title))
+      const rows = props.list.map((val, index) => {
+        // table row
+        return h('tr', {
+          key: index,
+          class: itemClasses(val, index),
+          onClick: e => {
+            e.stopPropagation()
+            itemSelect(val)
+          },
+          onMouseenter: () => setItemHighlight(isPicked(val) ? -1 : index)
+        }, props.tbColumns.map((col, idx) => { // table cells
+          return h('td', {
+            key: idx,
+            innerHTML: renderColumn(val, col)
+          })
+        }))
+      })
+
+      return h('table', { class: 'sp-table' }, [
+        // table thead
+        h('thead', h('tr', { class: { 'sp-rtl': rtl } }, thCells)),
+        // table tbody
+        h('tbody', { onMouseleave: () => setItemHighlight(-1) }, rows)
+      ])
     }
   }
 }
