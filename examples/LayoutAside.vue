a2e46d0a12b10d35148d9adb9a075f3946a642ec
Terry Zeng <terry5@foxmail.com>
2023-10-02 20:38:45 +0800

    feat: demo layout

--- examples/LayoutAside.vue
+++ examples/LayoutAside.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="px-3 pt-3 border-end">
     <div
-      class="text-secondary text-opacity-50 px-3 py-2"
-      :class="itemClasses(item)"
+      class="text-secondary text-opacity-50 px-3 py-2 fw-bold"
       style="cursor: pointer;"
-      v-for="item in forms"
+      :class="itemClasses(item)"
       :key="item.name"
+      v-for="item in forms"
       v-text="item.name"
       @click="switchForm(item, router)"
     />
@@ -21,6 +21,6 @@ const router = useRouter()
 
 function itemClasses (item) {
   if (item.code !== form.value) return ''
-  return 'text-body text-opacity-100 bg-body-secondary rounded-3'
+  return 'text-black text-opacity-100 bg-body-secondary bg-opacity-50 rounded-4'
 }
 </script>
--- examples/LayoutHeader.vue
+++ examples/LayoutHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg bg-light">
+  <nav class="navbar navbar-expand-lg bg-secondary-subtle">
     <div class="container-fluid">
       <a
         class="navbar-brand fs-4 fw-bold ps-3"
